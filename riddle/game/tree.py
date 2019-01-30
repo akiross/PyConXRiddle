1735a6693e4165c407baa1b6f0f235fd9c0c3c3e
Alessandro Re <ale@ale-re.net>
2019-01-30 22:44:05 +0100

    Fixed table with tree and users

--- riddle/game/tree.py
+++ riddle/game/tree.py
@@ -1,20 +1,65 @@
-from riddle.utils import get_level_structure
+from jinja2 import Template
+from riddle.utils import get_level_structure, query_user_process
+
+
+template = r"""
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Tree</title>
+    <meta name="description" content="The Game Tree">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <h1>Levels completed by users</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>Level\User</th>
+                {% for user in users %}
+                <th>{{ user }}</th>
+                {% endfor %}
+            </tr>
+        </thead>
+        <tbody>
+            {% for level, users in levels %}
+            <tr>
+                <td>{{ level }}</td>
+                {% for user in users %}
+                <td>{{ 'x' if user else '' }}</td>
+                {% endfor %}
+            </tr>
+            {% endfor %}
+        </tbody>
+    </table>
+</body>
+</html>
+"""
+
 
 def entry():
     return user_progress(), False
-    return "<br/>\n".join(f"<a href='{l}'>{l}</a>" for l in get_level_structure()), False
 
 
 def user_progress():
     """Return a table of levels solved by each user."""
+    # Query all progress from all users
+    tpl = Template(template)
 
-    # Get users
-    users = ['a', 'b', 'c']
-    # Build table
-    table = []
-    for l in get_level_structure():
-        row = [f"<span>{l}</span>"]
-        for u in users:
-            row.append(f"<span>User result</span>")
-        table.append("".join(row))
-    return "<br/>\n".join(table)
+    # Prepare data to be visualized
+    levels = dict()  # Dict level -> {user solved}
+    users = set()  # Set of users
+    for user_id, solved_level in query_user_process(None):
+        users.add(user_id)
+        levels.setdefault(solved_level, set()).add(user_id)
+
+    # Build table of which user solved which level
+    solved = []
+    users = sorted(users)  # Sort all users
+    ls = get_level_structure()
+    ls.remove(__name__.split('.')[-1])  # Remove this special module
+    for l in ls:
+        solved.append((l, [u in levels.get(l, {}) for u in users]))
+
+    return tpl.render(users=users, levels=solved)
