9a1f138e3ff672aa3ff7f030a434cdf2c7401308
Marco Bianchi <mbianchi.akita8@gmail.com>
2019-02-04 21:25:02 +0100

    added setup.py and bumpversion configuration

---
+++ setup.py
@@ -0,0 +1,25 @@
+import sys
+from setuptools import setup, find_packages
+from pkg_resources import parse_requirements
+
+
+with open("requirements.txt", 'r') as inst_reqs:
+    install_requires = [req.split(';')[0] for req in inst_reqs.read().split('\n') if not req.startswith('-')]
+
+with open("requirements-dev.txt", 'r') as test_reqs:
+    tests_require = [req.split(';')[0] for req in test_reqs.read().split('\n')  if not req.startswith('-')]
+
+packages = find_packages(include=['riddle', 'riddle.*'])
+
+setup(
+    name='riddle',
+    version='0.1.0-dev',
+    url='https://github.com/akiross/PyConXRiddle',
+    license='LICENSE',
+    description='riddle framework',
+    long_description=__doc__,
+    packages=packages,
+    install_requires=install_requires,
+    tests_require=tests_require,
+    include_package_data=True,
+)
