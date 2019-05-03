2edb16b56f71638e54349bc4fbe2986fe23adeda
Alessandro Re <ale@ale-re.net>
2019-05-03 11:10:39 +0200

    Fixed broken mathjax

--- riddle/game/wasp10/__init__.py
+++ riddle/game/wasp10/__init__.py
@@ -39,6 +39,7 @@ env = Environment(
             <meta charset="utf-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>{% block title %}{% endblock %}</title>
+            <script src='https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-MML-AM_CHTML' async></script>
             <!-- https://motherfuckingwebsite.com/ -->
             </head>
             <body>
